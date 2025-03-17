8a282aa0fca6b1319ee05312075daa79c6382d2f
sachit079 <sachitkumarbs21@gmail.com>
2025-03-17 22:43:57 +0530

    Revert the first comit
    This reverts commit 3232f4663e1a7cf2467c44cf89343a64d037be6f.

--- src/main/java/com/skn/repository/ContactRepo.java
+++
@@ -1,9 +0,0 @@
-package com.skn.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.skn.entities.Contact_details;
-
-public interface ContactRepo extends JpaRepository<Contact_details,String>{
-
-}
